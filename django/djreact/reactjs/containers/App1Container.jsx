a2d1c859364dbe4ced4d05d62657f1f5cf903cea
Shem Sedrick <ssedrick1@gmail.com>
2017-01-16 14:26:00 -0700

    Through step 6, but fab doesn't work

--- django/djreact/reactjs/containers/App1Container.jsx
+++ django/djreact/reactjs/containers/App1Container.jsx
@@ -8,7 +8,7 @@ export default class App1Container extends React.Component {
       <div className="container">
         <div className="row">
           <div className="col-sm-12">
-            <Headline>Working App!</Headline>
+            <Headline>Working App with server.js!</Headline>
           </div>
         </div>
       </div>
