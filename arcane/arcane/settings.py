9974cbd186fb563246b1017ae12c7aee06b36840
Shem Sedrick <ssedrick1@gmail.com>
2017-01-29 02:16:34 -0700

    Messed with the settings to get PostgreSQL working

--- arcane/arcane/settings.py
+++ arcane/arcane/settings.py
@@ -104,8 +104,14 @@ WSGI_APPLICATION = 'arcane.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'arcane_api',
+        'USER': 'django',
+        'PASSWORD': 'django-framework',
+        'HOST': 'localhost',
+        'PORT': '',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
