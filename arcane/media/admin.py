f33c1c78537fec64471910e0c7b46ce3702269ef
Tyler Scott <tybugman@hotmail.com>
2017-01-30 19:01:19 -0700

    added setup script

--- arcane/media/admin.py
+++ arcane/media/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import *
+admin.site.register(Genre)
+admin.site.register(Artist)
+admin.site.register(Album)
+admin.site.register(Song)
