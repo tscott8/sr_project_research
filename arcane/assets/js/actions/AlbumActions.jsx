ddaac8b625a3c2382afce87a2043915d7cf00acd
Shem Sedrick <ssedrick1@gmail.com>
2017-02-23 13:18:52 -0700

    Editing action types to hit the new api url

--- arcane/assets/js/actions/AlbumActions.jsx
+++ arcane/assets/js/actions/AlbumActions.jsx
@@ -1,7 +1,7 @@
 import * as types from '../constants/ActionTypes';
 
 export function getAlbums() {
-  return fetch("http://localhost:8000/browse/albums").then(response => response.json()).then(json => ({
+  return fetch("http://localhost:8000/api/albums").then(response => response.json()).then(json => ({
             type:types.GET_ALBUMS,
             albums: json
           }));
---
+++ arcane/assets/js/actions/AudioActions.jsx
@@ -0,0 +1,78 @@
+import * as types from '../constants/ActionTypes'
+
+export function retrieveSongs() {
+  // This could be written as a thunk or saga
+  return fetch("http://localhost:8000/api/tracks").then(response => response.json()).then(json => ({
+            type:types.GET_TRACKS,
+            tracks: json
+         }));
+};
+
+
+export function play(audio) {
+  if (audio.paused)
+      audio.play();
+  else
+      audio.pause();
+
+  return { type: types.PLAY, audio }
+}
+
+export function pause(audio) {
+  audio.pause();
+  return { type: types.PAUSE, audio }
+}
+
+function resetAudio(audio) {
+  // need to reset the song if it's the same file
+  audio.currentTime = 0;
+  const src = audio.src;
+  audio.src = null;
+  audio.src = src;
+}
+
+export function next(audio) {
+  resetAudio(audio)
+  return { type: types.NEXT, audio }
+}
+
+export function previous(audio) {
+  resetAudio(audio)
+  return { type: types.PREVIOUS, audio }
+}
+
+export function updateVolume(audio, volume) {
+  audio.volume = volume/100;
+  return { type: types.UPDATE_VOLUME, volume }
+}
+
+export function setTime(audio) {
+  const percent = audio.currentTime / audio.duration;
+  return { type: types.SET_TIME, audio }
+}
+
+export function setProgress(audio) {
+  return { type: types.SET_PROGRESS, audio }
+}
+
+export function setError(audio) {
+  return { type: types.ERROR, audio }
+}
+
+export function updatePosition(audio, percent) {
+  audio.currentTime = percent * audio.duration;
+  return { type: types.UPDATE_POSITION, audio }
+}
+
+export function toggleFavorite(id) {
+  return { type: types.TOGGLE_FAVORITE, id }
+}
+
+export function toggleRepeat() {
+  return { type: types.TOGGLE_REPEAT }
+}
+
+export function toggleLoop(audio) {
+  audio.loop = !audio.loop;
+  return { type: types.TOGGLE_LOOP, audio }
+}
