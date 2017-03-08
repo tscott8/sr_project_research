9df5d9552e59b78a77750c2c9205819094630d7a
Tyler Scott <tybugman@hotmail.com>
2017-03-07 17:59:28 -0700

    settings page added but not functional

--- arcane/assets/js/routes/routes.jsx
+++ arcane/assets/js/routes/routes.jsx
@@ -9,7 +9,7 @@ import MyMusic from '../containers/MyMusicPage'
 import Browse from '../containers/BrowsePage'
 import About from '../containers/AboutPage'
 import SplashPage from '../containers/SplashPage'
-
+import Settings from '../containers/SettingsPage'
 
 export default (
    <Route path="/" component={SplashPage}>
@@ -20,6 +20,7 @@ export default (
          <Route path="my_music" component={MyMusic} />
          <Route path="browse" component={Browse} />
          <Route path="about" component={About} />
+         <Route path="settings" component={Settings} />
          <Route path="*" component={SplashPage} />
       </Route>
       <Route path="*" component={SplashPage} />
